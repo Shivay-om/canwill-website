3e37ec332f6130532e8f00de92e645ab40451591
shivay-om <14.sachintyagi@gmail.com>
2025-08-08 20:51:28 +0530

    changes added

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -9,13 +9,23 @@ const Contact = () => {
       <div className=' flex flex-col justify-center items-center'>
         <h1 className='text-2xl text-center tracking-wide font-medium'>Start a conversation about your project today!</h1>
 
-        <Link to='/getQuote' onClick={() => {
-          window.scrollTo(0, 0);
-        }}>
-          <button className='mt-8 border border-black rounded-sm hover:bg-black hover:text-white bg-transparent bg-opacity-50 p-2 px-4'>Connect With Us
-            <FontAwesomeIcon icon={faGreaterThan} className='ml-2' />
-          </button>
-        </Link>
+        <div className=' lg:gap-6 flex flex-col justify-center items-center lg:flex-row'>
+          <Link to='/getQuote' onClick={() => {
+            window.scrollTo(0, 0);
+          }}>
+            <button className='mt-8 border border-black rounded-sm hover:bg-black hover:text-white bg-transparent bg-opacity-50 p-2 px-4'>Connect With Us
+              <FontAwesomeIcon icon={faGreaterThan} className='ml-2' />
+            </button>
+          </Link>
+
+          <Link to='/portfolio' onClick={() => {
+            window.scrollTo(0, 0);
+          }}>
+            <button className='mt-8 border border-black rounded-sm hover:bg-black hover:text-white bg-transparent bg-opacity-50 p-2 px-4'>Checkout Our Portfolio
+              <FontAwesomeIcon icon={faGreaterThan} className='ml-2' />
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   )
